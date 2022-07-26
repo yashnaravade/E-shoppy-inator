c28b6b98d70787c4be1dc4fd82b003f362667494
Vaibhavihole31 <itspinki05@gmail.com>
2022-07-26 15:34:16 +0530

    Create API of Login ad signup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,74 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Auth = require("./model/Auth/Auth");
+const cors = require('cors');
 
 const PORT = 5000;
 
 const app = express();
 
+app.use(express.json());
+
+app.use(cors());
+
+mongoose.connect('mongodb+srv://vaibhavi:vaibhavi@learnmongodb.eluyb.mongodb.net/?retryWrites=true&w=majority', ()=>{
+    console.log('connected to mongodb');
+});
+
+app.get('/health', (req, res) => {
+    res.json({
+      success: true,
+    });
+})
+
+app.post('/register',async(req,res)=>{
+  const auth = new Auth({
+    name :req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    mobile:req.body.mobile
+  })
+
+  await auth.save();
+  res.send({
+    message:"sign-up successfully"
+  })
+})
+
+app.post('/login',async(req,res)=>{
+  const auth = new Auth({
+    email : req.body.email,
+    password : req.body.password
+  })
+
+  await auth.save();
+  res.send({
+    message:"Login successfully"
+  })
+})
+
+// app.post('/signup',async(req,res) => {
+
+//     const {name, email, password,mobile} = req.body
+  
+//     const newUser = new User({
+//         name : name,
+//         email: email,
+//         password: password,
+//         mobile:mobile
+//     })
+  
+//     const savedUser= await  newUser.save();
+  
+//     res.json({savedUser});
+//   })
+
+
+
 app.listen(PORT , () => {
     console.log(`Server is listening on port ${PORT}`);
     })
+
+
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Homepage from './Component/Homepage/Homepage';
 import Login from './Auth/Login/Login';
 import Signup from './Auth/Signup/Signup';
 
 function App() {
   return (
-    <div >
-      {/* <Homepage/> */}
-      {/* <Login/> */}
-      <Signup/>
+    <div  className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Signup/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
